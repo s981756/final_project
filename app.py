8eb9ceeafe91ea268dfd4cc48ed1d6973ba236d0
s981756 <s981756@student.knoxschools.org>
2026-05-03 04:11:30 +0000

    stuff

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import pytest
 from other.fetch_data import glass_types_data
 #importing make_plot() function
 from other.making_plots import make_plot
+#importing build_model() function
+from other.ml_model import build_model
 
-from other.program import get_elements
+# from other.program import get_elements
 
 import pandas as pd
 import seaborn as sns
@@ -14,14 +16,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 
-#running fetch_data
+# #running fetch_data
 # glass_types_data()
 
 # #running function to make different plots
 # make_plot('Na', 'Si')
 # make_plot('Na', 'Mg')
 # make_plot('Na', 'Al')
-
-make_plot("Ba", "Fe")
+# make_plot("Ba", "Fe")
 
 # get_elements()
+
+# #running function to use ml_model
+build_model()
--- other/program.py
+++ other/program.py
@@ -1,2 +1,5 @@
-def get_elements():
-    print("Please type what elements (Ex. Al, Mg, Si, etc.) you have the concentrations of from your glass sample.")
+class Glass():
+    def __init__(self):
+
+    def build_model():
+        
